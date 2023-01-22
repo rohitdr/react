37e3ecc4a498ad68632a5d7cd61b262f95a19603
rohit <rohitdr098@gmail.com>
2023-01-22 11:17:45 +0530

    creating dimissial alert in navbar in 1.5s

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import React, {useState} from 'react'
+import Navbar from './Components/Navbar';
+import Acordian from './Components/Acordian';
+import Alert from './Components/Alert';
+
 function App() {
+const[mode, setMode]= useState("light")
+const[text, setText]=useState("light Mode")
+const[color, setColor]= useState("dark")
+const[alert, setAlert]=useState(null)
+const showAlert =(msg, type)=>{
+setAlert({
+  message : msg,
+ type : type,},
+ setTimeout(()=>{
+    setAlert(null)
+ }, 1500)
+
+ 
+
+
+)
+}
+const changeMode=()=>{
+  if(mode === 'light'){
+setText("light Mode")
+setColor("white")
+setMode("dark")
+document.body.style.backgroundColor = "grey"
+document.body.style.color = "white"
+showAlert("The dark mode has been activated","success" )
+  }
+  else{
+    setText("dark Mode")
+    setColor("dark")
+    setMode("light")
+  
+    document.body.style.backgroundColor = "white"
+    showAlert("The light mode has been activated","success")
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+<>
+
+
+
+<Navbar mode ={mode} text={text} changeMode={changeMode} color ={color}/>
+<Alert alert={alert} />
+<Acordian  color ={color} />
+</>
+  
   );
 }
 
